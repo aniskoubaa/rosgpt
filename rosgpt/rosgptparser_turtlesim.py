babdaf53a082e8b5778891a813986f44093cecec
Anis Koubaa <anis.koubaa@gmail.com>
2023-04-23 16:32:54 +0300

    fixes

--- rosgpt/rosgptparser_turtlesim.py
+++ rosgpt/rosgptparser_turtlesim.py
@@ -18,6 +18,11 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist 
 from turtlesim.msg import Pose
 import time
+from rclpy.executors import SingleThreadedExecutor
+import threading
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
 
 
 class TurtlesimController(Node):
@@ -31,9 +36,14 @@ class TurtlesimController(Node):
         self.y  = 0.0
         self.theta  = 0.0
         self.pose = Pose()
-        self.executor = rclpy.executors.SingleThreadedExecutor()
-        self.executor.add_node(self)
-        #rclpy.spin_once(self)
+
+        self.thread_executor = ThreadPoolExecutor(max_workers=1)
+
+        self.move_executor = SingleThreadedExecutor()
+        move_thread = threading.Thread(target=self.move_executor.spin)
+        move_thread.start()
+        print('ROSGPT Turtlesim Controller Started. Waiting for input commands ...')
+
         
     
     def pose_callback(self, msg):
@@ -41,72 +51,91 @@ class TurtlesimController(Node):
         self.y  = msg.y
         self.theta  = msg.theta
         self.pose = msg
-        print('-\n')
-        #print (self.pose)
+        #print (self.x) #used for debegging
     
-
+    #this callback represents the ROSGPTParser. It takes a JSON, parses it, and converts it to a ROS 2 command
     def voice_cmd_callback(self, msg):
         #print(msg.data)
-        cmd = json.loads(msg.data)
-        cmd = json.loads(cmd['json']) #we only consider the pure json message. cmd['text'] contains a mix of text and json
-        print('JSON command received: \n',cmd,'\n')
-        if cmd['action'] == 'go_to_goal':
-            location = cmd['params']['location']['value']
-            self.go_to_goal(location)
-        elif cmd['action'] == 'move':
-            linear_speed = cmd['params'].get('linear_speed', 0.2)
-            distance = cmd['params'].get('distance', 1.0)
-            is_forward = cmd['params'].get('is_forward', True)
-            self.move(linear_speed, distance, is_forward)
-        elif cmd['action'] == 'rotate':
-            angular_velocity = cmd['params'].get('angular_velocity', 1.0)
-            angle = cmd['params'].get('angle', 90.0)
-            is_clockwise = cmd['params'].get('is_clockwise', True)
-            self.rotate(angular_velocity, angle, is_clockwise)
+        try:
+            cmd = json.loads(msg.data)
+            cmd = json.loads(cmd['json']) #we only consider the pure json message. cmd['text'] contains a mix of text and json
+            print('JSON command received: \n',cmd,'\n')
+            if cmd['action'] == 'go_to_goal':
+                location = cmd['params']['location']['value']
+                self.go_to_goal(location)
+            elif cmd['action'] == 'move':
+                linear_speed = cmd['params'].get('linear_speed', 0.2)
+                distance = cmd['params'].get('distance', 1.0)
+                is_forward = cmd['params'].get('is_forward', True)
+
+                #METHOD 1: running an async method as a new task. But method 2 is more straightforward
+                #self.move_executor.create_task(self.move_coro(linear_speed, distance, is_forward))
+                
+                #METHOD 2. create a thread executor
+                #we need to run the method on a different thread to avoid blocking rclpy.spin. 
+                self.thread_executor.submit(self.move, linear_speed, distance, is_forward)
+
+                #running move on the main thread will generate to error, as it will block rclpy.spin
+                #self.move(linear_speed, distance, is_forward)
+            elif cmd['action'] == 'rotate':
+                angular_velocity = cmd['params'].get('angular_velocity', 1.0)
+                angle = cmd['params'].get('angle', 90.0)
+                is_clockwise = cmd['params'].get('is_clockwise', True)
+                self.thread_executor.submit(self.rotate, angular_velocity, angle, is_clockwise)
+                #self.rotate(angular_velocity, angle, is_clockwise)
+        except json.JSONDecodeError:
+            print('[json.JSONDecodeError] Invalid or empty JSON string received:', msg.data)
+        except Exception as e:
+            print('[Exception] An unexpected error occurred:', str(e))   
 
     def go_to_goal(self, location):
         # TODO: Implement go_to_goal method
+        # wil be defined later
         pass
 
+    async def move_coro(self, linear_speed, distance, is_forward):
+        return self.move(linear_speed, distance, is_forward)
 
     def get_distance(self,start, destination):
         return math.sqrt(((destination.x-start.x)**2 + (destination.y-start.y)**2))
     
-    def move (self, linear_speed, distance, is_forward):
-        if is_forward: 
-             direction = 'forward'
+    def move(self, linear_speed, distance, is_forward):
+        if is_forward:
+            direction = 'forward'
         else:
-             direction = 'backward'
-        print('Start moving the robot ',direction,' at ', linear_speed, 'm/s and for a distance ',distance,'meter')
-        #rclpy.spin_once(self)
-        twist_msg=Twist()
-        
-        if (linear_speed>1.0) :
+            direction = 'backward'
+        print('Start moving the robot ', direction, ' at ', linear_speed, 'm/s and for a distance ', distance, 'meter')
+        twist_msg = Twist()
+
+        if (linear_speed > 1.0):
             print('[ERROR]: The speed must be lower than 1.0!')
             return -1
-                
+
         twist_msg.linear.x = float(abs(linear_speed) if is_forward else -abs(linear_speed))
         twist_msg.linear.x = float(abs(linear_speed) * (1 if is_forward else -1))
 
         start_pose = copy.copy(self.pose)
-        
-        #rclpy.spin_once(self)
 
-        while self.get_distance (start_pose, self.pose)<distance:
-            print ('start_pose', start_pose, 'self.pose', self.pose, 'moved_distance: ', self.get_distance (start_pose, self.pose))
-            print('self.get_distance (start_pose, self.pose)<distance: ', self.get_distance (start_pose, self.pose)<distance)
-            
-            #rclpy.spin_once(self)
+        #self.move_executor.add_node(self)
+
+        while self.get_distance(start_pose, self.pose) < distance:
+            print('start_pose', start_pose, 'self.pose', self.pose, 'moved_distance: ', self.get_distance(start_pose, self.pose))
+            print('self.get_distance (start_pose, self.pose)<distance: ', self.get_distance(start_pose, self.pose) < distance)
+
             self.velocity_publisher.publish(twist_msg)
-            self.executor.spin_once()
-            time.sleep(1.0)
+            self.move_executor.spin_once(timeout_sec=0.1)
 
         twist_msg.linear.x = 0.0
         self.velocity_publisher.publish(twist_msg)
-        print ('start_pose', start_pose, 'self.pose', self.pose, 'distance: ', self.get_distance (start_pose, self.pose))
+        print('start_pose', start_pose, 'self.pose', self.pose, 'distance: ', self.get_distance(start_pose, self.pose))
         print('The Robot has stopped...')
 
-        return 0
+        #self.move_executor.remove_node(self)
+
+
+        #return 0
+
+
 
     def rotate (self, angular_speed_degree, desired_relative_angle_degree, clockwise):
         print('Start Rotating the Robot ...')
@@ -142,48 +171,9 @@ class TurtlesimController(Node):
         self.velocity_publisher.publish(twist_msg)
         print('The Robot has stopped...')
 
-        return 0
+        #return 0
 
-    def go_to_goal(self, target_x:float, target_y:float, distance_error_tolerance=0.5):
-        twist_msg = Twist()
-
-        while (True):
-            
-            p_gain_linear = 1.0 
-            distance = abs(math.sqrt(((float(target_x)-self.pose.get_x()) ** 2) + ((float(target_y)-self.pose.get_y()) ** 2)))
-            linear_speed = distance * p_gain_linear
-
-            p_gain_angular = 2.0
-            desired_angle_goal = math.atan2(target_y-self.pose.get_y(), target_x-self.pose.get_x())
-            angular_speed = (desired_angle_goal-self.pose.get_theta())*p_gain_angular
-
-            twist_msg.linear.x = linear_speed
-            twist_msg.angular.z = angular_speed
-            
-            #-----____PUBLISHING----------------------
-            #rclpy.spin_once(self)
-            self.velocity_publisher.publish(twist_msg)
-            time.sleep(0.1)
-            #---------------------------------------------
-
-            
-
-
-            if (distance <distance_error_tolerance):
-                print('Goal reached. Mission completed.')
-                #rclpy.spin_once(self)
-                twist_msg.linear.x = 0.0
-                twist_msg.angular.z = 0.0
-                #-----____PUBLISHING TO STOP----------------------
-                
-                self.velocity_publisher.publish(twist_msg)
-                time.sleep(0.1)
-                #---------------------------------------------
-                break
-        print('distance = ', distance)
-        print ('angle error = ', (desired_angle_goal-self.pose.get_theta()))
-        return 0
-
+    
 def main(args=None):
     rclpy.init(args=args)
     node = TurtlesimController()
